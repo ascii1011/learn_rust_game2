1d98a6506980d2095a1f70f94e992eb51d78edff
lapcharty <ascii1011@gmail.com>
2022-11-06 14:52:59 -0500

    added toml packages, imports for main, and game init

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,29 @@
 
+#![allow(unused)]
+use ggez::{Context, ContextBuilder, GameResult, GameError};
+use ggez::graphics::{self, Color};
+use ggez::event::{self, EventHandler};
+use ggez;
+use ggez::input::keyboard::{self, KeyCode};
 
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
+use std::{io};
+use std::collections::HashMap;
+use rand::{self, thread_rng,Rng};
+//use serde_derive;
+use serde::{Deserialize, Serialize};
+//use serde_json::{Result, Value};
+
+
+fn main() -> GameResult {
+    // Make a Context.
+    let (mut ctx, event_loop) = ggez::ContextBuilder::new("Rusty Silly Wars", "Chris Harty")
+        .build()
+        .expect("aieee, could not create ggez context!");
+
+    Ok(())
+    
 }
